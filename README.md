b37817ae842367c1733f774715b8b62d3dcf7d74
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2018-03-16 06:05:43 +0900

    Add simple install script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dotfiles
 dotfiles based on Linux Mint 18 Sarah
+
+## Installation
+
+~~~ sh
+$ git clone git@github.com:kaznag/dotfiles.git ~/.dotfiles
+$ sh ~/.dotfiles/script/setup.sh
+~~~
---
+++ script/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf ~/.dotfiles/.bash_logout ~/.bash_logout
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.profile ~/.profile
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
