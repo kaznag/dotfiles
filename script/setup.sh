ccb3366e825edfcc3b816a4d729e6dc48f559959
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2018-04-04 15:58:20 +0900

    Add nodebrew installation script to setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# install nodebrew
+if [ ! -d ~/.nodebrew ]; then
+    curl -L git.io/nodebrew | perl - setup
+fi
+
 ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/.dotfiles/.bash_logout ~/.bash_logout
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
