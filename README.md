0d020d59ed678cd6a4cdf4ccf8f160472d143ec8
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2020-04-08 21:55:38 +0900

    Update setup script

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ dotfiles based on Linux Mint 18 Sarah
 ## Installation
 
 ~~~ sh
-$ git clone git@github.com:kaznag/dotfiles.git ~/.dotfiles
-$ sh ~/.dotfiles/script/setup.sh
+$ git clone git@github.com:kaznag/dotfiles.git
+$ sh ~/dotfiles/script/setup.sh
 ~~~
--- script/setup.sh
+++ script/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
-ln -sf ~/.dotfiles/.bash_logout ~/.bash_logout
-ln -sf ~/.dotfiles/.bashrc ~/.bashrc
-ln -sf ~/.dotfiles/.profile ~/.profile
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/.bash_logout ~/.bash_logout
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.profile ~/.profile
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
